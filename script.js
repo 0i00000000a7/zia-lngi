c46e7e6bd669fecd27d52ca83ffdfdd27f80d13c
0i00000000a7 <byd255brkdn@hotmail.com>
2024-07-15 16:36:26 +0800

    nothing

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var t = 2000;
+var now = Date.now()
 if (localStorage.zia_lngi) t = Number(localStorage.zia_lngi)
-var final = "寄,寄,2,,1,,3";
+var final = "寄 #2世界";
 
 var bits = [
   [[""],[""]],
@@ -23,6 +24,20 @@ var bits = [
   [["寄,寄,("],["),2,,1,,2"]],
   [["寄,寄,1,("],["),,1,,2"]],
   [["寄,("],["),,1,,3"]],
+  [["寄,寄,("],["),,1,,3"]],
+  [["寄,3,,1,,("],[")"]],
+  [["寄,寄,,1,,("],["),2"]],
+  [["寄,寄,,1,,("],["),3"]],
+  [["寄,寄,,1,,1,("],[")"]],
+  [["寄,("],["),,1,,,,1,,2"]],
+  [["寄,("],["),,2,,2"]],
+  [["X<sup>"],["</sup>"]],
+  [["寄REE("],[")"]],
+  [["寄?("],[")"]],
+  [["SCG("],[")"]],
+  [["寄&%("],[")"]],
+  [["Rayo("],[")"]],
+  [["寄*~#@("],[")"]],
 ];
 
 var limits = [
@@ -47,6 +62,20 @@ var limits = [
   320000,
   330000,
   350000,
+  360000,
+  400000,
+  420000,
+  430000,
+  450000,
+  475000,
+  500000,
+  550000,
+  551000,
+  575000,
+  576000,
+  750000,
+  751000,
+  1000000,
 ];
 
 var offsets = [
@@ -70,6 +99,20 @@ var offsets = [
   1000,
   1500,
   1600,
+  2000,
+  1600,
+  2000,
+  1333.3334,
+  1666.6667,
+  1333.3334,
+  1333.3334,
+  1333.3334,
+  1333.3334,
+  0,
+  1857.142858,
+  0,
+  2000,
+  0,
 ];
 
 function findLimit(n){
@@ -88,7 +131,13 @@ function LRemainder(n){
   return ((n-limits[i-1])/(limits[i]-limits[i-1]))*(limits[i]-offsets[i-1])+offsets[i-1];
 }
 
-function parse(n){
+function parse(n,first=false){
+  if (first) {
+    let norm = parse(n)
+    if (n >= 5e5 & n < 5.5e5) norm = "数阵大小：" + norm
+    if (n >= 550000 & n < 551000) norm = ">" + norm
+    return norm
+  }
   if(findLimit(n) == 0){
     return Math.floor(1/(1-(n/2000)))-1;
   } else if(n < limits[limits.length-1]) {
@@ -99,10 +148,11 @@ function parse(n){
 }
 
 setInterval(function(){
-  if (t < (limits[limits.length - 1])) t++;
-  t+=9
+  if (t < (limits[limits.length - 1])) t+= (Date.now() - now) / 10;
+  t = Math.min(t,limits[limits.length - 1])
   localStorage.zia_lngi = t
-  document.getElementById("num").innerHTML = parse(t);
+  document.getElementById("num").innerHTML = parse(t,true);
+  now = Date.now()
 },10);
 
 function setTime(x) {
